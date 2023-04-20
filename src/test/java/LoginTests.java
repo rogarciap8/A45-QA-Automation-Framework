3330fe958e3c320a98b192d757309c1a29043b55
rogarciap8 <130935020+rogarciap8@users.noreply.github.com>
2023-04-20 18:30:06 -0400

    Verifying registration link takes you to registration page
    It does.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,31 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
-    @Test
-    public static void loginEmptyEmailPasswordTest() {
-
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
+public class LoginTests extends BaseTest{
+    //@Test
+    public static void registrationNavigation(){
+        if (System.getProperty("os.name").toLowerCase().contains("win")){
+            System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        }
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://bbb.testpro.io/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement regLink = driver.findElement(By.cssSelector("button[type='submit']"));
+        regLink.click();
+
+        //WebElement register = driver.findElement(By.cssSelector(""));
+        String regURL = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(),regURL);
         driver.quit();
     }
 }
+
