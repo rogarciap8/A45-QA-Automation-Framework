6e29fbd3859ea2b3b832bee9db97ae7e107e373b
rogarciap8 <130935020+rogarciap8@users.noreply.github.com>
2023-05-10 16:05:28 -0400

    Homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,21 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
-import java.util.UUID;
 
 public class BaseTest {
 
@@ -17,10 +24,10 @@ public class BaseTest {
     static WebDriverWait wait;
     Actions action;
 
+/*
     @BeforeSuite
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
+    public static void setupClass() {WebDriverManager.chromedriver().setup();}
+*/
 
     @DataProvider(name="IncorrectLoginData")
     public static Object[][] getDataFromDataProviders() {
@@ -34,12 +41,19 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL) {
-        //      Added ChromeOptions argument below to fix websocket error
+    public void launchBrowser(String BaseURL) throws MalformedURLException {
+/*
+      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
+*/
+        try {
+            driver = pickBrowser(System.getProperty("browser"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
         navigateToPage();
@@ -55,6 +69,34 @@ public class BaseTest {
 
     public static void navigateToPage() {
         driver.get(url);
+    }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.162:4444";
+
+        switch (browser){
+            case "firefox": //gradle clean test -Dbrowser=firefox
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "Microsoft Edge": //gradle clean test -Dbrowser=Microsoft Edge
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "grid-firefox": //gradle clean test -Dbrowser=grid-firefox
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome": //gradle clean test -Dbrowser=grid-chrome
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-edge": //gradle clean test -Dbrowser=grid-edge
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(options);
+        }
     }
 
     /*
---
+++ src/test/java/Homework24.java
@@ -0,0 +1,24 @@
+import Pages.HomePage;
+import Pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework24 extends BaseTest{
+    @Test
+    public void rename(){
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        String myNewPl = "Renamed Playlist";
+
+        //GIVEN
+        //loginPage.login();
+        loginPage.provideEmail("raul.garcia@testpro.io").providePassword("te$t$tudent").clickSubmit();
+
+        //WHEN
+        homePage.renamePl(myNewPl);
+
+        //THEN
+        Assert.assertTrue(homePage.verifyPlaylistExists(myNewPl));
+    }
+}
